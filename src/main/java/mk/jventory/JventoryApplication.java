e02d8264271b21bd9cff755fec4544f6e5c2cbb5
Miroslav Kálal <53705108+miroslav-kalal@users.noreply.github.com>
2021-08-05 22:22:07 +0200

    Add Spring Boot dependencies and JventoryApplication class

--- src/main/java/mk/jventory/JventoryApplication.java
+++ src/main/java/mk/jventory/JventoryApplication.java
@@ -1,9 +1,13 @@
 package mk.jventory;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JventoryApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(JventoryApplication.class, args);
     }
 
 }
